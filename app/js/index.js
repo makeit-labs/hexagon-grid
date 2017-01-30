0aec39d32c5b49ab64b98b1d84870b234d4aa817
Liam Morley <liam@carpeliam.com>
2017-01-29 22:50:58 -0500

    Display grid of hexagons
    - design is currently hardcoded
    - all hexagons rotated at same degree
    
    Signed-off-by: Shanfan Huang <chitchy@gmail.com>

--- app/js/index.js
+++ app/js/index.js
@@ -1,29 +1,45 @@
-import paper, { Point, Size, Rectangle, Color, Path } from 'paper';
+import paper, { Point, Size, Color, Path, Group } from 'paper';
 
-window.onload = () => {
-  const canvas = document.querySelector('canvas');
-  paper.setup(canvas);
+const { RegularPolygon, Rectangle } = Path;
 
-  var topLeft = new Point(10, 20);
-  var rectSize = new Size(200, 100);
-  var rect = new Rectangle(topLeft, rectSize);
+const canvas = document.querySelector('canvas');
+paper.setup(canvas);
 
+const size = 30;
+const dimensions = { x: 12, y: 12 };
 
-
-  var renderedRect = new Path.Rectangle(rect);
-
-  renderedRect.style = {
-    fillColor: 'black',
-    strokeColor: 'red'
+function pointFor({x, y}, radius) {
+  const countOfEvenRows = Math.floor(y / 2);
+  const countOfOddRows = y - countOfEvenRows;
+  let yPoint = 2 * radius * countOfOddRows + radius * countOfEvenRows;
+  let xPoint = (radius * 2 * Math.sqrt(3/4)) * x;
+  if (!!(y % 2)) {
+    xPoint += size * Math.sqrt(3/4);
+    yPoint -= size / 2;
   }
-
-
-  // var myCircle = new Path.Circle(new Point(100, 70), 50);
-  // myCircle.fillColor = 'black';
-
-
-
-  paper.view.draw();
-
-  console.log(paper.view);
+  return new Point(xPoint + 50, yPoint + 50);
 }
+
+for (let x = 0; x < dimensions.x; x++) {
+  for (let y = 0; y < dimensions.y; y++) {
+    let center = pointFor({x, y}, size);
+    const leftTop = center.subtract({x: Math.sqrt(3/4) * size, y: 1/2 * size });
+    const rightBottom = center.add({x: Math.sqrt(3/4) * size, y: size });
+
+    const hexagon = new RegularPolygon(center, 6, size);
+    hexagon.style = {
+      strokeWidth: 1,
+      strokeColor: 'black',
+      fillColor: '#fffccc'
+    };
+
+    const rect = new Rectangle(leftTop, rightBottom);
+    rect.fillColor = '#222';
+
+    const mask = new RegularPolygon(center, 6, size);
+    const group = new Group(mask, rect);
+    group.clipped = true;
+  }
+}
+
+paper.view.draw();
